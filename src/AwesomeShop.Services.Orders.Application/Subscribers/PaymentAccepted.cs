56d85b8a400c2b8bac3a79dd6444f52a3859b0d0
Willian Raphael <willianr.matos@outlook.com>
2022-05-11 18:50:16 -0400

    Adicionar PaymentAccepted

---
+++ src/AwesomeShop.Services.Orders.Application/Subscribers/PaymentAccepted.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AwesomeShop.Services.Orders.Application.Subscribers
+{
+    public class PaymentAccepted
+    {
+        public Guid Id { get; set; }
+        public string FullName { get; private set; }
+        public string Email { get; private set; }
+    }
+}
--- src/AwesomeShop.Services.Orders.Application/Subscribers/PaymentAcceptedSubscriber.cs
+++ src/AwesomeShop.Services.Orders.Application/Subscribers/PaymentAcceptedSubscriber.cs
@@ -1,5 +1,11 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using AwesomeShop.Services.Orders.Core.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using System;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -7,9 +13,67 @@ namespace AwesomeShop.Services.Orders.Application.Subscribers
 {
     internal class PaymentAcceptedSubscriber : BackgroundService
     {
+        private readonly IServiceProvider _serviceProvider;
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
+        private const string Queue = "order-service/payment-accepted";
+        private const string Exchange = "order-service";
+        private const string RoutingKey = "payment-accepted";
+
+        public PaymentAcceptedSubscriber(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+            var connectionFactory = new ConnectionFactory
+            {
+                HostName = "localhost"
+            };
+            
+            _connection = connectionFactory.CreateConnection("order-service-payment-accepted-subscriber");
+            
+            _channel = _connection.CreateModel();
+            
+            _channel.ExchangeDeclare(Exchange, "topic", true);
+            _channel.QueueDeclare(Queue, true, false, false, null);
+            _channel.QueueBind(Queue, "payment-service", RoutingKey);
+        }
+
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            throw new NotImplementedException();
+            var consumer = new EventingBasicConsumer(_channel);
+
+            consumer.Received += async (sender, eventArgs) => {
+                var byteArray = eventArgs.Body.ToArray();
+
+                var contentString = Encoding.UTF8.GetString(byteArray);
+                var message = JsonConvert.DeserializeObject<PaymentAccepted>(contentString);
+
+                Console.WriteLine($"Message PaymentAccepted received with Id {message.Id}");
+
+                var result = await UpdateOrder(message);
+
+                if (result)
+                    _channel.BasicAck(eventArgs.DeliveryTag, false);
+            };
+
+            _channel.BasicConsume(Queue, false, consumer);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task<bool> UpdateOrder(PaymentAccepted paymentAccepted)
+        {
+            using (var scope = _serviceProvider.CreateScope())
+            {
+                var orderRepository = scope.ServiceProvider.GetService<IOrderRepository>();
+
+                var order = await orderRepository.GetByIdAsync(paymentAccepted.Id);
+
+                order.SetAsCompleted();
+
+                await orderRepository.UpdateAsync(order);
+
+                return true;
+            }
         }
     }
 }
